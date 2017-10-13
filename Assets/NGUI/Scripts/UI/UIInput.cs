2b4b43a8e5f4951f15339220c76fae1279db65ab
PC016\Administrator <mopower77@naver.com>
2017-10-13 14:52:10 +0900

    -리소스 추가

--- Assets/NGUI/Scripts/UI/UIInput.cs
+++ Assets/NGUI/Scripts/UI/UIInput.cs
@@ -885,10 +885,11 @@ public class UIInput : MonoBehaviour
 
 		bool isMac = (
 			rp == RuntimePlatform.OSXEditor ||
-			rp == RuntimePlatform.OSXPlayer ||
-			rp == RuntimePlatform.OSXWebPlayer);
+			rp == RuntimePlatform.OSXPlayer );
+        //||rp == RuntimePlatform.OSXWebPlayer
 
-		bool ctrl = isMac ?
+
+        bool ctrl = isMac ?
 			((ev.modifiers & EventModifiers.Command) != 0) :
 			((ev.modifiers & EventModifiers.Control) != 0);
 
