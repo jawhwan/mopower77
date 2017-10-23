8510838a3a31ac544c1e35d6dc80118b258a7f46
PC016\Administrator <mopower77@naver.com>
2017-10-23 17:40:05 +0900

    -add

---
+++ Assets/Scripts/FaceList.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceList : MonoBehaviour {
+
+    public List<GameObject> list_Face;
+
+    private int n_Index;
+
+
+    private void Awake()
+    {
+        n_Index = 0;
+      
+    }
+
+
+    public void NextFace()
+    {
+        if(list_Face.Count - 1 == n_Index)
+        {
+
+            return;
+        }
+
+        ++n_Index;
+
+    }
+
+    public void PrevFace()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/UILogin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILogin : MonoBehaviour
+{
+
+    public void OnClickWarrior()
+    {
+
+    }
+
+    public void OnClickBow()
+    {
+
+    }
+
+}
