02196581b63e8ef0a469453a674fb2b80eca68a1
PC043\Administrator <mopower77@naver.com>
2017-10-24 18:53:24 +0900

    -로그인신 카메라 스크립트 ing

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    private Transform tf_target;
+    private float f_moveSpeed;
+
+    public void SetMove(Transform tf,float fSpeed)
+    {
+        tf_target = tf;
+        f_moveSpeed = fSpeed;
+    }
+
+
+
+
+}
